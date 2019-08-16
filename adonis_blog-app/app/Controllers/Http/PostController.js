121059ce93b76787064da7dc7e1761640d75a47c
Mish <mishuct26@gmail.com>
2019-08-16 14:45:45 +0200

    Demo how to add data in view, even as object

--- adonis_blog-app/app/Controllers/Http/PostController.js
+++ adonis_blog-app/app/Controllers/Http/PostController.js
@@ -1,8 +1,13 @@
 'use strict'
 
 class PostController {
-    async index() {
-        return 'Posts'
+    //pass view as a parameter
+    async index( { view }) {
+        //render the view the dot inside render method targets through inside folder
+        return view.render('posts.index', {
+            //Object to show and test you can add data in our view. Next, call it from inside the view
+            title: 'Latest Post'
+        })
     }
 }
 
